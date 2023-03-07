21a22a4afdaf95d005b8838e85c288b4b2cb17b7
illior <svinuhov.egor2000@mail.ru>
2023-03-07 10:54:56 +0500

    Player Controller4

--- Assets/PlayerController/Scripts/ThirdPersonController.cs
+++ Assets/PlayerController/Scripts/ThirdPersonController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    // Start is called before the first frame update
+	[Header("References")]
+	[SerializeField] private Transform orientation;
+	[SerializeField] private Transform player;
+	[SerializeField] private Transform playerObj;
+
+	[SerializeField] private float rotationSpeed = 7;
+
+    private float verticalInput, horizontalInput;
+
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+
+		orientation.forward = viewDir.normalized;
+
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(inputDir != Vector3.zero)
+        {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, rotationSpeed * Time.deltaTime);
+        }
     }
 }
